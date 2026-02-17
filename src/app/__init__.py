36e7cdc89ebdc3c8918a072d4747c52345b11bda
Спартак <spartakrozhaev@bk.ru>
2026-02-17 18:40:53 +0700

    Add run script and test route

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,5 +2,15 @@
 from flask import Flask
 
 def create_app():
+    """Фабрика приложения Flask"""
     app = Flask(__name__)
+
+    # Простой тестовый маршрут для проверки работы
+    @app.route('/')
+    def hello():
+        # Эта функция будет вызвана при обращении к корневому URL
+        return 'Hello, ERP Prologistics!'
+
+    # Здесь позже будут подключены blueprints, расширения и т.д.
+
     return app
